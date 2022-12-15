40f7b77194f33980003333bcfa823a14f89dd348
e-hokage777 <e.hokage777@gmail.com>
2022-12-15 12:21:56 +0000

    added password reset form

---
+++ curator/password_reset.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>easygo - curator password reset</title>
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <!-- Fontawesome css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- easygo css -->
+    <link rel="stylesheet" href="../assets/css/general.css">
+</head>
+
+<body>
+
+    <!-- main content start -->
+    <main class="form-page-main container">
+        <div class="img-container d-none d-lg-block">
+            <img src="../assets/images/svgs/login.svg" alt="register image">
+        </div>
+        <div class="form-container container">
+            <form>
+                <div class="form-header">
+                    <div class="logo">
+                        <img src="../assets/images/svgs/logo.svg" alt="easy go logo">
+                    </div>
+                    <p class="instruction">Please enter your email address</p>
+                </div>
+                <div class="input-field">
+                    <input type="text" placeholder="Email">
+                </div>
+                <!-- <div class="input-field">
+                    <div class="password-input-container">
+                        <input type="password" placeholder="Password">
+                        <button type="button" class="toggle-password-show"><i class="fa-sharp fa-solid fa-eye-slash"></i></button>
+                    </div>
+                    <div class="text-end">
+                        <a style="font-size: var(--font-size-4); color: var(--easygo-gray-1)"  href="#">Forgot Password?</a>
+                    </div>
+                </div> -->
+                <div class="input-field button-container">
+                    <button class="easygo-btn-1" type="submit">Reset Password</button>
+                    <!-- <a href="./register.php">Create an account</a> -->
+                </div>
+            </form>
+        </div>
+    </main>
+    <!-- main content end -->
+
+    <!-- Bootstrap js -->
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    <!-- JQuery js -->
+    <script src="../assets/js/jquery-3.6.1.min.js"></script>
+    <!-- easygo js -->
+    <script src="../assets/js/general.js"></script>
+</body>
+
+</html>
