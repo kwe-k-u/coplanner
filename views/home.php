9e7f167de94ea378115ae2f90756461530703194
e-hokage777 <e.hokage777@gmail.com>
2023-01-05 10:51:45 +0000

    having issue with git

--- views/home.php
+++ views/home.php
@@ -24,6 +24,7 @@
         <?php
         require_once("../components/navbar.php");
         ?>
+        
         <!-- navbar [end] -->
         <!--- ================================ -->
         <main>
