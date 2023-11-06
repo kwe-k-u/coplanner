cb7c7e12450ad9ee5af1a8f6eb13f5bbe4f54720
e-hokage777 <e.hokage777@gmail.com>
2023-11-06 22:42:10 +0000

    added coplanner stuff wb

---
+++ coplanner/coplanner_navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-md fixed-top easygo-nav-white">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./home.php">
+            <img class="logo-medium" src="../assets/images/site_images/logo.png" alt="">
+        </a>
+        <!-- <button class="navbar-toggler sidebar-toggler" type="button" data-target="main-sidebar">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="justify-content-end" id="mynavbar">
+            <div class='d-flex gap-4'>
+                <a href='../views/login.php' class='easygo-btn-4 border-blue text-blue easygo-fs-5'>Create an account</a>
+                <a href='../views/register.php' class='easygo-btn-5 bg-blue text-white easygo-fs-5'>Sign in</a>
+            </div>
+        </div>
+</nav>
