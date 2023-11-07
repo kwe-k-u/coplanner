972cfdd4d56ab5328a565063d949acf6beb806ee
e-hokage777 <e.hokage777@gmail.com>
2023-11-07 12:59:56 +0000

    done with coplanner home

--- coplanner/coplanner_home.php
+++ coplanner/coplanner_home.php
@@ -109,30 +109,241 @@ require_once(__DIR__ . "/../controllers/interaction_controller.php");
             <!--- Section 4 [start] -->
             <section class="my-5 py-5">
                 <h2 class="mb-4">View other itineraries created by other people</h2>
-                <div class="itinerary-cards-container">
-                    <div class="itinerary-card">
-                        <p class="itinerary-card-top-note">Pay to view</p>
-                        <div class="itinerary-card-body">
-                            <div class="price-and-people">
-                                <div>
-                                    GHC 500 <br>
-                                    Single day
-                                </div>
-                                <div>
-                                    3-5 People <br>
-                                    Single day
+                <nav>
+                    <div class="nav nav-tabs easygo-nav-tabs-alt" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="popular-selections-tab" data-bs-toggle="tab" data-bs-target="#nav-popular-selections" type="button" role="tab" aria-controls="nav-popular-selections" aria-selected="true">Popular Selections</button>
+                        <button class="nav-link" id="editors-pick-tab" data-bs-toggle="tab" data-bs-target="#nav-editors-pick" type="button" role="tab" aria-controls="nav-editors-pick" aria-selected="false">Editor's Pick</button>
+                        <button class="nav-link" id="solo-travels-tab" data-bs-toggle="tab" data-bs-target="#nav-solo-travels" type="button" role="tab" aria-controls="nav-solo-travels" aria-selected="false">Solo Travels</button>
+                        <button class="nav-link" id="budget-tours-tab" data-bs-toggle="tab" data-bs-target="#nav-budget-tours" type="button" role="tab" aria-controls="nav-budget-tours" aria-selected="false">Budget Tours</button>
+                        <button class="nav-link" id="family-friendly-tab" data-bs-toggle="tab" data-bs-target="#nav-family-friendly" type="button" role="tab" aria-controls="nav-family-friendly" aria-selected="false">Family Friendly</button>
+                    </div>
+                </nav>
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-popular-selections" role="tabpanel" aria-labelledby="popular-selections-tab">
+                        <div class="itinerary-cards-container">
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div>
-                                <h6 class="easygo-fw-1">Shared Itineries</h6>
-                                <p class="itinerary-desc">
-                                    An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
-                                </p>
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="itinerary-activities">
-                                <div class="activity">Hike</div>
-                                <div class="activity">Hike</div>
-                                <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="nav-editors-pick" role="tabpanel" aria-labelledby="editors-pick-tab">
+                        <div class="itinerary-cards-container">
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="nav-solo-travels" role="tabpanel" aria-labelledby="solo-travels-tab">
+                        <div class="itinerary-cards-container">
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="nav-budget-tours" role="tabpanel" aria-labelledby="budget-tours-tab">
+                        <div class="itinerary-cards-container">
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="nav-family-friendly" role="tabpanel" aria-labelledby="family-friendly-tab">
+                        <div class="itinerary-cards-container">
+                            <div class="itinerary-card">
+                                <p class="itinerary-card-top-note">Pay to view</p>
+                                <div class="itinerary-card-body">
+                                    <div class="price-and-people">
+                                        <div>
+                                            GHC 500 <br>
+                                            Single day
+                                        </div>
+                                        <div>
+                                            3-5 People <br>
+                                            Single day
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h6 class="easygo-fw-1">Shared Itineries</h6>
+                                        <p class="itinerary-desc">
+                                            An AI generated description of the itinerary that someone has created talking about the type of itinerary and teh activities
+                                        </p>
+                                    </div>
+                                    <div class="itinerary-activities">
+                                        <div class="activity">Hike</div>
+                                        <div class="activity">Hike</div>
+                                        <div class="text-gray-1 d-flex align-items-center">+3 more</div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
