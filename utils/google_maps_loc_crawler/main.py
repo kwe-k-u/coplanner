82a92629162f61a35a7fa9c5fceea48a5e600e1f
Kweku <kwekuappiah11@gmail.com>
2023-11-21 11:37:10 +0000

    run main.py and saved results to tuple.txt to avoid multiple redundant google calls

--- utils/google_maps_loc_crawler/main.py
+++ utils/google_maps_loc_crawler/main.py
@@ -7,9 +7,10 @@ import json
 import multiprocessing
 import threading
 from random import randint
-from time import sleep
+from time import sleep, time
 
-destination_types = ["amusement_park","aquarium","art_gallery","bowling_alley","cafe","campground","library","lodging","movie_theater","musem","night_club","park","casino","stadium","shopping_mall","restaurant","zoo","tourist_attraction"]
+destination_types = ["shopping_mall","restaurant","zoo","tourist_attraction"]
+# destination_types = ["amusement_park","aquarium","art_gallery","bowling_alley","cafe","campground","library","lodging","movie_theater","musem","night_club","park","casino","stadium","shopping_mall","restaurant","zoo","tourist_attraction"]
 search_queries = ["things to do","places to visit","tour sites"]
 max_km = 50
 ghana_tl = (11.02603066883424, -3.1447569426908735)
@@ -41,36 +42,37 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 
 
 def get_destinations(query,center, category):
-	# print("getting destination")
-	return {query: category}
 	save_data = {}
 	maps_api_key = ""
 	url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key="+maps_api_key+"&keyword="+query+"&location="+str(center[0])+", "+str(center[1])+"&type="+category+"&radius=50000"
 
 	payload = {}
 	headers = {}
-	response = requests.request("GET", url, headers=headers, data=payload)
-	data = response.json()["results"]
-	print("Checking for ",center)
-	print("Size", len(data))
-	for index in range(len(data)):
-		loc_info = data[index]
-		try:
-			relevant = {
-				"name" : loc_info["name"],
-				"place_id" : loc_info["place_id"],
-				"types" : loc_info["types"],
-				"user_ratings_total" : loc_info.get("user_ratings_total",0),
-				"rating" : loc_info.get("rating",1),
-				"vicinity" : loc_info["vicinity"],
-				"location" : loc_info["geometry"]["location"]
-			}
-		except:
-			save_file("error",loc_info)
-			input("Error caught")
-		save_data[index] = relevant
-		print("Added", relevant["name"])
+	try:
 
+		response = requests.request("GET", url, headers=headers, data=payload)
+		data = response.json()["results"]
+		print("Checking for ",center)
+		print("Size", len(data))
+		for index in range(len(data)):
+			loc_info = data[index]
+			try:
+				relevant = {
+					"name" : loc_info["name"],
+					"place_id" : loc_info["place_id"],
+					"types" : loc_info["types"],
+					"user_ratings_total" : loc_info.get("user_ratings_total",0),
+					"rating" : loc_info.get("rating",1),
+					"vicinity" : loc_info["vicinity"],
+					"location" : loc_info["geometry"]["location"]
+				}
+			except:
+				save_file("error",loc_info)
+				input("Error caught")
+			save_data[index] = relevant
+			print("Added", relevant["name"])
+	except:
+		save_file("url_fails",url)
 	return save_data
 
 
@@ -96,27 +98,21 @@ def move(cords, direction='right', distance=50):
 
 
 def next_line(cu, start):
-	# print("Next line")
 	# move down
 	current_position = move(cu,"bottom")
 	# go back to start position
 	return (current_position[0], start)
 
 def move_right(cu):
-	# print("Moving right")
 	return move(cu)
 
 
 def run_get_threads(position,typ,result):
 	for query in search_queries:
 		_ =get_destinations(query,position,typ)
-		result.append(_)
+		result.append((typ,_))
 
 def run_destination_search(vert_steps,hor_steps,position,typ):
-	wait_time = randint(1,3)
-	print("wait",wait_time)
-	sleep(wait_time)
-	# return {}
 	current_position = position
 	des = {}
 	num_threads = vert_steps * hor_steps
@@ -124,33 +120,22 @@ def run_destination_search(vert_steps,hor_steps,position,typ):
 	thread_result = []
 	print("Spawning",num_threads," threads for destination retrieval")
 	for i in range(num_threads):
-			t = threading.Thread(target=run_get_threads,args=(position,typ,thread_result))
-			threads.append(t)
-			t.start()
-			current_position = move_right(current_position)
+		t = threading.Thread(target=run_get_threads,args=(position,typ,thread_result))
+		threads.append(t)
+		t.start()
+		current_position = move_right(current_position)
 
-			if i%hor_steps == 0:
-				current_position = next_line(current_position,start_pos)
+		if i%hor_steps == 0:
+			current_position = next_line(current_position,start_pos)
 
 	for _ in threads:
 		_.join()
-
+	print("Threads finished",thread_result)
+	save_file("tuples/tuples",thread_result)
 	for _ in thread_result:
-		des.update(_)
-		
-	print("result_thread",len(thread_result))
-	# for i in thread_result:
-	# 	des.update(i)
-	# for v in range(vert_steps):
-	# 	for h in range(hor_steps):
-	# 		for query in search_queries:
-	# 			_ =get_destinations(query,current_position,typ)
-	# 			print("Get result",_)
-	# 			des.update(_)
-	# 		current_position = move_right(current_position)
-	# 	current_position = next_line(current_position,start_pos)
+		des.update(_[1])
 
-	return des
+	return (typ,des)
 
 
 def spin_thread(func,args = [],result_queue = None):
@@ -160,9 +145,9 @@ def spin_thread(func,args = [],result_queue = None):
 		result_queue.put(result)
 
 def save_file(filename,data):
-	print("saving file")
-	print(filename+".json",data)
-	return 0
+	# print("saving file")
+	# print(filename+".json",data)
+	# return 0
 	with open(filename+".json", 'a') as file:
 		json.dump(data, file, indent=4)
 	if filename == "error":
@@ -173,25 +158,27 @@ def save_file(filename,data):
 
 
 if  __name__ == "__main__":
+	start_time = time()
 
 
 	current_position = ghana_tl
 
 	width = calculate_distance(ghana_tl[0],ghana_tl[1],ghana_tr[0],ghana_tr[1])
 	height = calculate_distance(ghana_tl[0],ghana_tl[1],ghana_bl[0],ghana_bl[1])
-	# print("Ghana width and height", width,height)
 
 	v_steps = int(height / max_km) # vertical steps
 	h_steps = int(width / max_km) # horizontal steps
 
 	process_list = {}
-	process_results = {}
+	process_results = []
+	# process_results = {}
 	process_queue = multiprocessing.Queue()
 
 	print("Spinning up",len(destination_types), "pocesses")
 
-	for type_check in destination_types[:1]:
-		process_results[type_check] = []
+	for type_check in destination_types:
+		# process_results[type_check] = []
+		process_results = [{type_check : {}}]
 
 
 		# all_location_data = {}
@@ -200,16 +187,34 @@ if  __name__ == "__main__":
 		process_list[type_check] = process
 		process.start()
 
-	while len(process_results.keys()) != len(process_list.keys()):
-		type_check,_ = process_queue.get()
-		print("result check",type_check,_)
-		process_results[type_check] = _
+	while len(process_results) != len(process_list.keys()):
+	# while not all(process_results.values()):
+		a,b = process_queue.get()
+
+		# process_results[a] = b
+		for _ in range(len(process_results)):
+			c = process_results[_]
+			print("c result", c)
+			if (list(c.keys())[0] == a):
+				process_results[_][a] = b
+
 	for p in process_list.values():
-		# print(p)
 		p.join()
 
-	for key in process_results.keys():
-		res = process_results[key]
-		print(key,res)
-		# all_location_data.update(res)
-		# save_file(key,res)
+	for _ in process_results:
+		key = list(_.keys())[0]
+		res = _[key]
+		save_file(key,res)
+
+	end_time = time()
+	seconds = end_time - start_time
+	minutes = int(seconds / 60)
+	min_seconds = seconds %60
+	hours = int(minutes / 60)
+	hours_minutes = minutes % 60
+
+	print("Time stats")
+	print("seconds", seconds)
+	print("minutes",minutes,min_seconds)
+	print("hours",hours, hours_minutes)
+	print("total time", hours, hours_minutes, min_seconds)
