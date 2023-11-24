c662a6a62ec4475d86916f4263bded97f4f3d151
e-hokage777 <e.hokage777@gmail.com>
2023-11-24 12:50:42 +0000

    done updating scrollbars

--- coplanner/coplanner_home_3.php
+++ coplanner/coplanner_home_3.php
@@ -127,7 +127,7 @@ require_once(__DIR__ . "/../utils/core.php");
             <section class="container px-4 my-5 py-5">
                 <h2 class="mb-4">View other itineraries created by other people</h2>
                 <nav>
-                    <div class="nav nav-tabs easygo-nav-tabs-alt" id="nav-tab" role="tablist">
+                    <div class="nav nav-tabs easygo-nav-tabs-alt justify-content-center" id="nav-tab" role="tablist">
                         <button class="nav-link active" id="popular-selections-tab" data-bs-toggle="tab" data-bs-target="#nav-popular-selections" type="button" role="tab" aria-controls="nav-popular-selections" aria-selected="true">Popular Selections</button>
                         <button class="nav-link" id="editors-pick-tab" data-bs-toggle="tab" data-bs-target="#nav-editors-pick" type="button" role="tab" aria-controls="nav-editors-pick" aria-selected="false">Editor's Pick</button>
                         <button class="nav-link" id="solo-travels-tab" data-bs-toggle="tab" data-bs-target="#nav-solo-travels" type="button" role="tab" aria-controls="nav-solo-travels" aria-selected="false">Solo Travels</button>
@@ -137,7 +137,7 @@ require_once(__DIR__ . "/../utils/core.php");
                 </nav>
                 <div class="tab-content" id="nav-tabContent">
                     <div class="tab-pane fade show active" id="nav-popular-selections" role="tabpanel" aria-labelledby="popular-selections-tab">
-                        <div class="itinerary-cards-container">
+                        <div class="itinerary-cards-container easygo-scroll-bar scroll-h">
                             <div class="itinerary-card">
                                 <p class="itinerary-card-top-note">Pay to view</p>
                                 <div class="itinerary-card-body">
@@ -219,7 +219,7 @@ require_once(__DIR__ . "/../utils/core.php");
                         </div>
                     </div>
                     <div class="tab-pane fade" id="nav-editors-pick" role="tabpanel" aria-labelledby="editors-pick-tab">
-                        <div class="itinerary-cards-container">
+                        <div class="itinerary-cards-container easygo-scroll-bar scroll-h">
                             <div class="itinerary-card">
                                 <p class="itinerary-card-top-note">Pay to view</p>
                                 <div class="itinerary-card-body">
@@ -275,7 +275,7 @@ require_once(__DIR__ . "/../utils/core.php");
                         </div>
                     </div>
                     <div class="tab-pane fade" id="nav-solo-travels" role="tabpanel" aria-labelledby="solo-travels-tab">
-                        <div class="itinerary-cards-container">
+                        <div class="itinerary-cards-container easygo-scroll-bar scroll-h">
                             <div class="itinerary-card">
                                 <p class="itinerary-card-top-note">Pay to view</p>
                                 <div class="itinerary-card-body">
@@ -305,7 +305,7 @@ require_once(__DIR__ . "/../utils/core.php");
                         </div>
                     </div>
                     <div class="tab-pane fade" id="nav-budget-tours" role="tabpanel" aria-labelledby="budget-tours-tab">
-                        <div class="itinerary-cards-container">
+                        <div class="itinerary-cards-container easygo-scroll-bar scroll-h">
                             <div class="itinerary-card">
                                 <p class="itinerary-card-top-note">Pay to view</p>
                                 <div class="itinerary-card-body">
@@ -335,7 +335,7 @@ require_once(__DIR__ . "/../utils/core.php");
                         </div>
                     </div>
                     <div class="tab-pane fade" id="nav-family-friendly" role="tabpanel" aria-labelledby="family-friendly-tab">
-                        <div class="itinerary-cards-container">
+                        <div class="itinerary-cards-container easygo-scroll-bar scroll-h">
                             <div class="itinerary-card">
                                 <p class="itinerary-card-top-note">Pay to view</p>
                                 <div class="itinerary-card-body">
